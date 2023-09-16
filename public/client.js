5f64e5c9c24c6c4d40280af8648a6875f65cbc21
Snezoh97 <sinelizwitaku@gmail.com>
2023-09-16 20:58:31 +0200

    Done with the filter

--- public/client.js
+++ public/client.js
@@ -1,14 +1,9 @@
 $(document).ready(function () {
-    
-    $.get('/your-server-endpoint', function (data) {
- 
-      const moneyValue = "$1,600";
-      const depositValue = "Deposit money";
-  
-      //Content of the 'money' and 'deposit' elements with the new values
-     
-      $('#money').text(moneyValue);
-      $('#deposit').text(depositValue);
-    });
+  $.get("/rest-api", function (data) {
+    const moneyValue = "$1,600";
+    const depositValue = "Deposit money";
+
+    $("#money").text(moneyValue);
+    $("#deposit").text(depositValue);
   });
-  
+});
--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// Serve static files from the 'public' folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Define a route that responds with the data you want to display
-app.get('/your-server-endpoint', (req, res) => {
-  // Replace these with actual data retrieval logic
+app.get("/rest-api", (req, res) => {
   const buttonRegister = "Register";
   const moneyValue = "$1,600";
   const depositValue = "Deposit money";
 
-  // Respond with the data as JSON
-  res.json({ money: moneyValue, deposit: depositValue, buttonr: buttonRegister });
+  res.json({
+    money: moneyValue,
+    deposit: depositValue,
+    buttonr: buttonRegister,
+  });
 });
 
-// Start your Express server
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
